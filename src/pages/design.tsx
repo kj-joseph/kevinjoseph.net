0a030b3ec0dc10df0e4711ae9b878de416eecf44
Kevin Joseph <kjoseph@rainbird.com>
2018-12-26 19:08:50 -0800

    Fix linting issues

--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -18,12 +18,16 @@ export default class DesignPage<Props> extends React.Component<any> {
 			<div>
 				<h1>Graphic Design</h1>
 
-				<p>Besides my main vocation as a web developer, I also dabble in graphic design.
-					In early 2018, I launched a design side business: <a
-					href="http://kjbranded.com/" target="_blank" rel="noopener noreferrer">KJ Branded</a>.</p>
+				<p>
+					Besides my main vocation as a web developer, I also dabble in graphic design.
+					In early 2018, I launched a design side
+					business: <a href="http://kjbranded.com/" target="_blank" rel="noopener noreferrer">KJ Branded</a>.
+				</p>
 
-				<p>You can see more examples of my graphic design work at my <a
-					href="https://www.behance.net/-kj" target="_blank" rel="noopener noreferrer">portfolio on Behance</a>.</p>
+				<p>
+					You can see more examples of my graphic design work
+					at my <a href="https://www.behance.net/-kj" target="_blank" rel="noopener noreferrer">portfolio on Behance</a>.
+				</p>
 
 			</div>
 		);
