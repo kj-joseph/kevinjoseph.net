49be8d606f7898be75f326cba877a2381511f570
Kevin Joseph <kj.joseph@gmail.com>
2021-03-06 04:05:26 -0800

    Style tweaks

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,7 +21,7 @@ export default class Header extends React.Component<IHeaderProps> {
 				<div className="header__details">
 					<div className="header__name">Kevin Joseph</div>
 					<div className="header__title">Web Developer</div>
-					<div className="header__location">San Jose, CA</div>
+					<div className="header__location">San Jos&eacute;, CA</div>
 				</div>
 
 				<nav role="navigation">
